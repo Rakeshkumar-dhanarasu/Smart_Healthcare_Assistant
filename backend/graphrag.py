52701d96a21628f320726c299f9a792228220082
Rakeshkumar-dhanarasu <rakeshkumar.dhanarasu@gmail.com>
2026-06-08 20:22:21 +0530

    Initial commit without heavy dependencies

---
+++ backend/graphrag.py
@@ -0,0 +1,45 @@
+class GraphRetriever:
+    def __init__(self, api_key, prompt_path):
+        from neo4j import GraphDatabase
+        from google import genai
+        import os
+        from dotenv import load_dotenv
+
+        self.driver = GraphDatabase.driver(uri=os.getenv("NEO4J_URI"),auth=(os.getenv("NEO4J_USER"),os.getenv("NEO4J_PASS")))
+        self.client = genai.Client(api_key=api_key)
+        self.model = "gemini-2.5-flash"
+
+        with open(prompt_path, "r") as f:
+            self.prompt_template = f.read()
+
+    def _generate_cypher(self, query: str) -> str:
+        prompt = self.prompt_template + query
+
+        response = self.client.models.generate_content(
+            model=self.model,
+            contents=prompt
+        )
+
+        return response.text.strip()
+
+    def _run_cypher(self, cypher: str) -> list:
+        with self.driver.session() as session:
+            result = session.run(cypher)
+            return [record.data() for record in result]
+
+    def get_context(self, query: str) -> dict:
+        try:
+            cypher = self._generate_cypher(query)
+            data = self._run_cypher(cypher)
+
+            return {
+                "cypher": cypher,
+                "data": data,
+                "query": query
+            }
+
+        except Exception as e:
+            return {
+                "error": str(e),
+                "data": []
+            }
---
+++ ocr_functions/ocr_tester.py
@@ -0,0 +1,19 @@
+from paddleocr import PaddleOCR
+
+detect_model = PaddleOCR(
+    lang='en',
+    use_doc_orientation_classify=False, 
+    use_doc_unwarping=False, 
+    use_textline_orientation=False,
+    text_detection_model_name="PP-OCRv5_server_det",
+    # text_recognition_model_dir="./medical_ocr_v5_model",
+    text_recognition_model_name="PP-OCRv5_server_rec"
+    )
+
+image_path = "C:/Users/Rakesh Kumar/VSCode/Medical_Assistant/datasets/test_images/tester8.jpg" 
+
+result = detect_model.predict(image_path)
+
+# Print bounding boxes
+for res in result:  
+    print(res['rec_texts'])
